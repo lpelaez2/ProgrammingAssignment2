30bff53fb266d28436a8be6dcfa004b7d981a300
lpelaez2 <lpelaez2@gmail.com>
2014-06-12 06:34:27 -0500

    Comments
    Work directly on Github

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,93 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Calculates inverse of a matrix using R solve function and caching matrix 
+## and its inverse to improve computational time.
+##
+## Test function is provided if you want to use it
 
-## Write a short comment describing this function
+
+## makeCacheMatrix
+##   Inits environment variables (caching when needed) and defines associated functions 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # Inits inverse matrix
+  my_inverse <- NULL
+  
+  
+  # Inits my_matrix if a matrix is indicated, otherwise set it as null
+  if (is.null(x)) {
+    my_matrix <- NULL
+  }
+  else
+    my_matrix <- x
+  
+  
+  # SetMatrix function: inits matrix using argument and inits its inverse as null
+  setMatrix<-function(y){
+    my_matrix <<- y
+    my_inverse <<- NULL
+  }
+  
+  
+  # GetMatrix function: shows original matrix
+  getMatrix<-function() {
+    my_matrix
+  }
+  
+  
+  # SetInverse function: set inverse matrix using argument
+  setInverse<-function(x) {
+    my_inverse <<- x
+  }
+  
+  # GetInverse shows inverse matrix
+  getInverse <- function() {
+    my_inverse
+  }
+  
+  
+  # Define available functions 
+  list( setMatrix=setMatrix
+        , getMatrix=getMatrix
+        , setInverse=setInverse
+        , getInverse=getInverse
+      )
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: 
+##   Calculates inverse of matrix only if was not previously calculated (cached)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(my_matrix = matrix()) {
+  my_inverse <- my_matrix$getInverse()
+  
+  if(!is.null(my_inverse)){
+    message("Getting cached inverse")
+    return(my_inverse)
+  }
+  
+  my_inverse <- solve(my_matrix$getMatrix())
+  my_matrix$setInverse(my_inverse)
+  my_inverse
+}
+
+
+## testInverse01
+##   Function to test main functions setting original matrix at the begining . 
+##   Sample taken from http://www.mathcentre.ac.uk/resources/uploaded/sigma-matrices11-2009-1.pdf
+testInverse01 <- function() {
+  
+  my_matrixTest <- makeCacheMatrix( matrix(c(7,2,1, 0,3,-1, -3,4,-2), nrow=3, ncol=3, byrow = TRUE))
+  cacheSolve(my_matrixTest)
+  
+}
+
+## testInverse02
+##   Function to test main functions setting original matrix later. 
+##   Sample taken from http://www.purplemath.com/modules/mtrxinvr.htm
+testInverse02 <- function() {
+  
+  my_matrixTest <- makeCacheMatrix()
+  my_matrixTest$setMatrix(matrix(c(1,3,3, 1,4,3, 1,3,4), ncol=3, nrow=3, byrow = TRUE))
+  
+  cacheSolve(my_matrixTest)
+  
 }
